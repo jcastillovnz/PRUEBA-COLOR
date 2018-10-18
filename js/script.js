54f926e4eb9492c93f7fc57bfe5f79dd9ac7c6bc
Jose Castillo <k.y.258@hotmail.com>
2018-10-18 14:17:08 -0300

    5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const img= document.getElementById('imagen');
+const img= document.getElementById('mascara');
 const output= document.getElementById('output');
 
 img.addEventListener('click', function (e) {
@@ -25,62 +25,6 @@ img.addEventListener('click', function (e) {
 r=pixel[0] ;
 g=pixel[1] ;
 b=pixel[2] ;
-
-  function componentToHex(c) {
-
-  var hex = c.toString(16);
-
-  return hex.length == 1 ? "0" + hex : hex;
-
-}
-/*
-
-function changeRange() {
-// Get R,G,B values & Convert string into integer.
-var r = parseInt(document.getElementById("r").value);
-var g = parseInt(document.getElementById("g").value);
-var b = parseInt(document.getElementById("b").value);
- 
-// Generate color. Example: #20b9ff
-var color = "#" + hex(r) + hex(g) + hex(b);
- 
-// Change background color and text.
-document.body.style.backgroundColor = color;
-document.getElementById("hex-label").innerText = color;
-document.getElementById("r-label").innerText = r;
-document.getElementById("g-label").innerText = g;
-document.getElementById("b-label").innerText = b;
- 
-if (r < 100 && g < 100 && b < 100) {
-document.getElementById("container").style.color = "white";
-} else {
-document.getElementById("container").style.color = "black";
-}
-}
- 
-function hex(v) {
-// Get hexadecimal numbers.
-var hex = v.toString(16);
-if (v < 16) {
-hex = "0" + hex;
-}
-return hex;
-}
-
-*/
-
-
-
-
-function rgbToHex(r, g, b) {
-
- var hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-
-
-
-}
-
-alert(hex );
 
 
 
